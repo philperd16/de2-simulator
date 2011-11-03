39598328927a1dbbef48fc2097f4ef3e2e619b2b
guisgb13@gmail.com <guisgb13@gmail.com@826e861a-bc3e-401b-cd36-690a2eff8323>
2011-11-03 06:57:26 +0000

    Implemented values parsing.

--- DE2Simulator/src/simulator/InstructionThread.java
+++ DE2Simulator/src/simulator/InstructionThread.java
@@ -31,12 +31,101 @@ public class InstructionThread implements Runnable{
 				result += getVariableValue(current.getIdentifier());
 				steps.remove();
 			}
+			else if ( current.getType().equals(OperationElementType.VALUE) ){
+				result += getParsedValue(current.getIdentifier());
+				steps.remove();
+			}
 			else if ( current.getType().equals(OperationElementType.BINARY_OPERATOR) ){
+				//create a method doOperation for the Operator implementations, which receives both operands and returns result
+			}else if ( current.getType().equals(OperationElementType.UNARY_OPERATOR) ){
+				//create a method doOperation for the Operator implementations, which receives both operands and returns result
 			}
 		}
 		return -1;
 	}
 	
+	private int getParsedValue(String identifier) {
+		if ( !identifier.contains("'") ){
+			return Integer.parseInt(identifier);
+		}
+		else {
+			if ( Character.toLowerCase(identifier.charAt(identifier.indexOf("'"))) == 'b' ){
+				return parseBinaryString(identifier);
+			}
+			else if ( Character.toLowerCase(identifier.charAt(identifier.indexOf("'"))) == 'x' ){
+				return parseBinaryString(hexToBinaryString(identifier));
+			}
+			return -1;
+		}
+	}
+
+	private String hexToBinaryString(String identifier) {
+		String hex = identifier.split("'")[1];
+		StringBuilder binary = new StringBuilder();
+		for ( int i=0; i<hex.length(); i++ ){
+			if ( Character.toLowerCase(hex.charAt(i)) == '0' ){
+				binary.append("0000");
+			}
+			else if ( Character.toLowerCase(hex.charAt(i)) == '1' ){
+				binary.append("0001");
+			}
+			else if ( Character.toLowerCase(hex.charAt(i)) == '2' ){
+				binary.append("0010");
+			}
+			else if ( Character.toLowerCase(hex.charAt(i)) == '3' ){
+				binary.append("0011");
+			}
+			else if ( Character.toLowerCase(hex.charAt(i)) == '4' ){
+				binary.append("0100");
+			}
+			else if ( Character.toLowerCase(hex.charAt(i)) == '5' ){
+				binary.append("0101");
+			}
+			else if ( Character.toLowerCase(hex.charAt(i)) == '6' ){
+				binary.append("0110");
+			}
+			else if ( Character.toLowerCase(hex.charAt(i)) == '7' ){
+				binary.append("0111");
+			}
+			else if ( Character.toLowerCase(hex.charAt(i)) == '8' ){
+				binary.append("1000");
+			}
+			else if ( Character.toLowerCase(hex.charAt(i)) == '9' ){
+				binary.append("1001");
+			}
+			else if ( Character.toLowerCase(hex.charAt(i)) == 'a' ){
+				binary.append("1010");
+			}
+			else if ( Character.toLowerCase(hex.charAt(i)) == 'b' ){
+				binary.append("1011");
+			}
+			else if ( Character.toLowerCase(hex.charAt(i)) == 'c' ){
+				binary.append("1100");
+			}
+			else if ( Character.toLowerCase(hex.charAt(i)) == 'd' ){
+				binary.append("1101");
+			}
+			else if ( Character.toLowerCase(hex.charAt(i)) == 'e' ){
+				binary.append("1110");
+			}
+			else if ( Character.toLowerCase(hex.charAt(i)) == 'f' ){
+				binary.append("1111");
+			}
+		}
+		return new String(binary);
+	}
+
+	public int parseBinaryString(String identifier) {
+		int result = 0;
+		int k = 1;
+		String binary = identifier.split("'")[1];
+		for ( int i=binary.length(); i>-1; i-- ){
+			result += k * Integer.parseInt(""+binary.charAt(i));
+			k = k*2;
+		}
+		return result;
+	}
+
 	private int getVariableValue(String identifier) {
 		for ( Variable var : environmentVariables ){
 			if ( var.getName().equals(identifier) ){
